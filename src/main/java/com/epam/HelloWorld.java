7175faa1d81147888c53b9ce761d2e5bd18914bd
Ravil.Ianbekov <ravil.ianbekov@epicgames.com>
2022-07-28 08:47:07 +0300

    Add google search scenarios

--- src/main/java/com/epam/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.epam;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
--- src/test/java/com/epam/scenarios/AbstractTest.java
+++ src/test/java/com/epam/scenarios/AbstractTest.java
@@ -6,15 +6,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AbstractTest {
 
-    private WebDriver driver;
+    private static WebDriver driver;
 
     @BeforeAll
     public static void precondition (){
         System.out.println("Setup driver property");
         System.setProperty("webdriver.chrome.driver","/Users/Ravil_Yanbekov/Documents/chromedriver");
+        getDriver();
     }
 
-    public WebDriver getDriver() {
+    private static WebDriver getDriver() {
         if (driver == null) {
             return new ChromeDriver();
         }
