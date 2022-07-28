7e14741266b5a8b68fe7533e175f79414866e7c5
Ravil.Ianbekov <ravil.ianbekov@epicgames.com>
2022-07-28 13:20:18 +0300

    Add google search scenarios

---
+++ src/test/java/com/epam/scenarios/SearchingBarTest.java
@@ -0,0 +1,41 @@
+package com.epam.scenarios;
+
+import com.epam.pages.GoogleResultsPage;
+import com.epam.pages.GoogleSearchPage;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SearchingBarTest extends BaseTest {
+
+    private static GoogleSearchPage googleSearchPage;
+    private static GoogleResultsPage googleResultsPage;
+
+    @BeforeEach
+    public void navigateToResource() {
+        googleSearchPage = new GoogleSearchPage(getDriver());
+        googleResultsPage = new GoogleResultsPage(getDriver());
+        googleSearchPage.navigateTo(googleSearchPage.PAGE_URL);
+        assertTrue(googleSearchPage.getTitle().contains("Google"));
+    }
+
+    @Test
+    public void testSearchAndPressEnter() {
+        googleSearchPage.searchAndPressEnter("Webdriver");
+        assertTrue(googleResultsPage.getTitle().contains("webdriver - Google Search"));
+    }
+
+    @Test
+    public void testSearchAndPressSearchButton() {
+        googleSearchPage.searchAndPressSearchButton("Webdriver");
+        assertTrue(googleResultsPage.getTitle().contains("webdriver - Google Search"));
+    }
+
+    @Test
+    public void testClearButtonOnSearchBar() {
+        googleSearchPage.typeSearchQueryInSearchBar("Webdriver");
+        googleSearchPage.clearSearchBar();
+        assertTrue(googleSearchPage.getTitle().contains("Google"));
+    }
+}
